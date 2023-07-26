ec5a133ed1cd7cb83bf332848a997565ef9f1605
Domitius <133902698+recenasu@users.noreply.github.com>
2023-07-25 21:41:36 -0700

    added variables for html elements

--- assets/js/script-1.js
+++ assets/js/script-1.js
@@ -1,5 +1,10 @@
 // This file contains the js code for getting the song lyrcis from the Genius API
 
+// Variables for interacting with html elements (will need to add more once we get it working)
+var song1Title = document.getElementById('placeholder1a');
+var song1Artist = document.getElementById('placeholder1b');
+var song1Lyrics = document.getElementById('placeholder1c');
+
 // **Draft function wrapper for getLyricsAPI function
 // function getLyricsAPI() {move end bracket to the bottom when uncommented}
 
@@ -15,10 +20,15 @@ fetch(lyricsURL)
     .then(function (data) {
         // console log for viewing full api data
         console.log(data);
-        // for loop and console log for viewing specific keys from the fetched data
+        // for loop and console log for viewing specific keys from the fetched data (the below ".license" is a placeholder for now)
         for (var i = 0; i < data.length; i++) {
             console.log(data[i].license);
         }
+        // TO DO: set the text content of the html elements to the values pulled from the API (the user's top ten song titles, artists, and lyrics). watch video and review lessons. These are placeholders. We will need to add these for each song once we get this working.
+        song1Title.textContent = data[i].something1;
+        song1Artist.textContent = data[i].something2;
+        song1Lyrics.textContent = data[i].something3;
+
     })
 
 // **Draft code for get lyrics button
