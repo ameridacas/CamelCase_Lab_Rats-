31aec5fe33c5241ee6d4d6d3c11bfc7f61bcde65
Domitius <133902698+recenasu@users.noreply.github.com>
2023-07-25 19:43:14 -0700

    initial js code for get lyrics

--- assets/js/script-1.js
+++ assets/js/script-1.js
@@ -1,1 +1,15 @@
 // Code for the song lyrics will goes here...
+
+// variables 
+// placeholder lyrics URL variable
+var lyricsURL = 'https://api.github.com/orgs/nodejs/repos';
+
+
+// fetch request from Genius
+fetch(lyricsURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+    })
