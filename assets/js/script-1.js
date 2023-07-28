66d4e24f16c8aaa76048aaedaa6c19e273a4a269
Domitius <133902698+recenasu@users.noreply.github.com>
2023-07-28 16:43:25 -0700

    added code for storing past views in local storage

--- assets/js/script-1.js
+++ assets/js/script-1.js
@@ -1,5 +1,25 @@
 // This file contains the js code for seeing how many views a user-specified subject has had on Wikipedia in a ten day period.
 
+// Variable for adding up total views
+var totalViews = 0
+// Array for storing past views
+var pastViewsArray = []
+
+// This function saves the historical view data from previous subjects.
+function saveViewData() {
+    // If nothing exists in local storage, add the totalViews to the pastViewsArray, stringify the array, and store it in local storage
+    if (localStorage.getItem("storedViews") === null) {
+        pastViewsArray.unshift(totalViews);
+        localStorage.setItem("storedViews", JSON.stringify(pastViewsArray));
+    } else {
+        // If views exist in local storage, parse the stored data into the pastViewsArray, then add the totalViews to the beginning of the array, then stringify the array and store it in local storage.
+        pastViewsArray = JSON.parse(localStorage.getItem("storedViews"));
+        pastViewsArray.unshift(totalViews);
+        console.log(pastViewsArray);
+        localStorage.setItem("storedViews", JSON.stringify(pastViewsArray));
+    }
+}
+
 
 // This is a test variable representing the subject item selected by the user or taken from the movie api
 var subject = "Guardians of the Galaxy";
@@ -25,8 +45,7 @@ fetch(viewsURL)
         // variable and console for viewing full array data (developer tool only)
         var subjectViews = viewsData;
         console.log(subjectViews)
-        // Variable for adding up total views
-        var totalViews = 0
+        
 
         for (i = 0; i < 10; i++) {
             console.log(viewsData.items[i].views)
@@ -36,6 +55,7 @@ fetch(viewsURL)
 
         // Display the total views for the ten day period in the console
         console.log(totalViews);
+        saveViewData()
 
     })
 
